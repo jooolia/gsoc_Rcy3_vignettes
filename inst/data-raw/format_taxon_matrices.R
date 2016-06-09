7259d97faac5d35eb4b3e16e2339e379522acdd7
Julia Gustavsen <j.gustavsen@gmail.com>
2016-06-09 17:38:44 +0200

    update reading in species co-occurence so that weights are automatically associated with graph.

--- inst/data-raw/format_taxon_matrices.R
+++ inst/data-raw/format_taxon_matrices.R
@@ -23,7 +23,8 @@ prok_matrix <- prok_virus_matrix[!(rownames(prok_virus_matrix) %in% rownames(vir
 
 ## need to remove the filter and unclassified
 prok_matrix <- prok_matrix[!grepl("_filter", rownames(prok_matrix)),]
-prok_matrix <- prok_matrix[!grepl("unclass", rownames(prok_matrix)),]
+prok_matrix <- prok_matrix[!grepl("unclass",
+                                  rownames(prok_matrix)),]
 
 
 ## only 19 sites in common. Let's say that rows have to be in at least 30% of them
@@ -85,9 +86,11 @@ test_row_by_row[upper.tri(test_row_by_row)] <- NA
 ## now remove those with less than 0.6 
 melted_rows <- melt(test_row_by_row)
 melted_together <- na.omit(melted_rows)
-filtered_row_by_row <- filter(melted_together, abs(value) > 0.8)
 
-no_self_filtered_row_by_row <- filter(filtered_row_by_row, value != 1)
+names(melted_together)[3] <- "weight"
+filtered_row_by_row <- filter(melted_together, abs(weight) > 0.8)
+
+no_self_filtered_row_by_row <- filter(filtered_row_by_row, weight != 1)
 
 write.table(no_self_filtered_row_by_row,"../../data/virus_prok_cor.tsv",
             sep="\t")
